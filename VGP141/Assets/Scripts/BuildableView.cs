52b378334c48496652c1267f59d54057edb49adb
gmanuelTFS <gmanuel@torontofilmschool.ca>
2023-07-27 20:51:31 -0400

    Finished #21
    - Created buildable data object and 2 instances
    - Created buildable view class

---
+++ VGP141/Assets/Scripts/BuildableView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VGP23U
+{
+    public class BuildableView : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }   
+}
